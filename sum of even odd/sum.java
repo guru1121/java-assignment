c89aa2680efdd0eaa57a12f59d85da4aeb7ce38c
guru1121 <gurunandm@gmail.com>
2022-10-30 22:52:46 +0530

    even odd sum

---
+++ sum of even odd/sum.java
@@ -0,0 +1,19 @@
+
+class sum {
+    public static void main(String[] args) {
+        int[] arr = { 5, 8, 7, 9, 1, 3, 2 };
+        int n = arr.length - 1;
+        int E_sum = 0, O_sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] % 2 == 0) {
+                E_sum += arr[i];
+            } else {
+                O_sum += arr[i];
+            }
+        }
+        System.out.println(E_sum);
+        System.out.println(O_sum);
+    }
+
+}
