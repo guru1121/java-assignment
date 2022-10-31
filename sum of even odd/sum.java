b240761df9ddc6936abed3b147ac9d574d58ec90
Gurunand Mourya <116895145+guru1121@users.noreply.github.com>
2022-10-31 11:00:33 +0530

    Update sum.java

--- sum of even odd/sum.java
+++ sum of even odd/sum.java
@@ -2,7 +2,7 @@
 class sum {
     public static void main(String[] args) {
         int[] arr = { 5, 8, 7, 9, 1, 3, 2 };
-        int n = arr.length - 1;
+        int n = arr.length;
         int E_sum = 0, O_sum = 0;
 
         for (int i = 0; i < n; i++) {
